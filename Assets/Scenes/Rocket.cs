19a3bc7723f446f64d59bf1e5eb17fa54f8ff85c
Simon <simon.wardle@outlook.com>
2019-01-19 23:03:55 +0000

    Added level 2 and logic to move from 1 to 2 if you die in 1 restart if you die in 2 restart at 2

--- Assets/Scenes/Rocket.cs
+++ Assets/Scenes/Rocket.cs
@@ -1,17 +1,25 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
     [SerializeField] float rcsThrust = 100f;
     [SerializeField] float mainThrust = 50f;
+
     Rigidbody rigidBody;
     AudioSource audioSource;
+
+    enum State { Alive, Dying, Transcending}
+    State state = State.Alive;
+    int currentLevel = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
+        Scene scene = SceneManager.GetActiveScene();
+        currentLevel = scene.buildIndex;
     }
 
     // Update is called once per frame
@@ -53,5 +61,29 @@ public class Rocket : MonoBehaviour
         }
 
         rigidBody.freezeRotation = false;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                break;
+            case "Finish":
+                Invoke("LoadNextScene", 1f);
+                break;
+            default:
+                state = State.Dying;
+                print(currentLevel);
+                SceneManager.LoadScene(currentLevel);
+                break;
+        }
+    }
+
+    private void LoadNextScene()
+    {
+        currentLevel = 1;
+        print("In LoadNextScene " + currentLevel);
+        SceneManager.LoadScene(currentLevel);
     }
 }
