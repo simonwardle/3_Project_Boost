c86f7887f66a121be31278274bb0904eb6fc7b1e
Simon Wardle <simon.wardle@outlook.com>
2019-01-21 17:16:47 +0000

    Added movement to obstacle

---
+++ Assets/Scenes/Oscillator.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Oscillator : MonoBehaviour
+{
+    [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
+    [SerializeField] float period = 2f;                                     //2 seconds
+    [Range(0, 1)] [SerializeField] private float movementFactor;
+
+    private Vector3 startingPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float cycles = Time.time / period; //grows continually from 0
+
+        const float tau = Mathf.PI * 2f;  //about 6.28 or 2 PI
+        float rawSinWave = Mathf.Sin(cycles * tau);
+
+        movementFactor = rawSinWave / 2f + 0.5f;
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPosition + offset;
+    }
+}
